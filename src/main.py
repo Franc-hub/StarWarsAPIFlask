a17f47c99f70c4a959070b7d31b146c60b3b3e8f
Franc Cosp <franccospmeyaeso@gmail.com>
2021-06-03 16:15:28 +0000

    exercise do it

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 This module takes care of starting the API Server, Loading the DB and Adding the endpoints
 """
 import os
+import json
 from flask import Flask, request, jsonify, url_for
 from flask_migrate import Migrate
 from flask_swagger import swagger
@@ -20,6 +21,37 @@ db.init_app(app)
 CORS(app)
 setup_admin(app)
 
+todos = [
+    { "label": "My first task", "done": False },
+    { "label": "My second task", "done": False }
+]
+
+@app.route('/', methods=['GET'])
+def hello_world():
+    return "<h1>That's  my first API using FLASK and PYTHON</h1>"
+
+
+@app.route('/todos', methods=['GET'])
+def all_todos ():
+    return jsonify(todos), 200
+
+
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    request_body = request.data
+    print("Incoming request with the following body", request_body)
+    decoded_object = json.loads(request_body)
+    print("decoded object: ", decoded_object)
+    todos.append(decoded_object)
+    return jsonify(todos)
+
+
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    print("This is the position to delete: ",position)
+    print("You delete a todo", todos[position])
+    del todos[position]
+    return jsonify(todos)
 # Handle/serialize errors like a JSON object
 @app.errorhandler(APIException)
 def handle_invalid_usage(error):
--- src/models.py
+++ src/models.py
@@ -17,3 +17,9 @@ class User(db.Model):
             "email": self.email,
             # do not serialize the password, its a security breach
         }
+
+
+class Todo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    label = db.Column(db.String(120), unique=True, nullable=False)
+    done = db.Column(db.Boolean(), unique=False, nullable=False)
